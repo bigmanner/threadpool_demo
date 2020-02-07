a8dc2636d569c6a495aa39ff4478a0cb8fa73168
Troy Sun <1090256107@qq.com>
2020-02-07 16:21:09 +0800

    original version(thread in main,instead of in class)

---
+++ test3/ReadTarget.h
@@ -0,0 +1,26 @@
+#pragma once
+class ReadTarget//设定为结构体的类型
+{
+private:
+	char instrument_id[31] = "";//合约名称
+	int dir = 0;//买卖方向
+	int vol = 0;//买卖数量
+public:
+	ReadTarget()
+	{
+
+	}
+	ReadTarget(char* para_id, int d, int v)
+	{
+		dir = d;
+		vol = v;
+		for (int i = 0; i < 31; i++)
+		{
+			instrument_id[i] = para_id[i];
+		}
+	}
+	void output()
+	{
+		std::cout << "instrument id:" << instrument_id << " dir:" << dir << " vol:" << vol << std::endl;
+	}
+};
--- test3/test3.cpp
+++ test3/test3.cpp
@@ -1,21 +1,73 @@
-﻿// test3.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include<iostream>
+#include<thread>
+#include<mutex>
+#include<condition_variable>
+#include"mqueue.h"//将队列添加进来
+#include"ReadTarget.h"//将任务的最小单位添加进来
 
-#include "pch.h"
-#include <iostream>
+std::mutex mtx;
+std::condition_variable cv;
+
+int num = 0;//share value by producer and consumer，相当于一个信号量,记录队列当中的任务的个数
+SafeQueue<ReadTarget>  safequeue;
+//消费者线程，什么叫做消费者线程                                                                                                      
+void consumer()
+{
+	while (true)//consumer一直在运转
+	{
+		int tem_num;
+		ReadTarget rt;
+		{//同步机制
+			std::unique_lock<std::mutex> lck(mtx);
+			while (num == 0)
+			{
+				cv.wait(lck);
+			}
+			tem_num = num;
+			safequeue.dequeue(rt);
+			num--;
+		}
+		//同步机制结束，子线程单独运行，处理逻辑
+		std::cout << "线程id：" << std::this_thread::get_id() << " ";//check
+		std::cout << "输出的num为：" << tem_num << " ";//check
+		rt.output();
+		//逻辑运行结束，准备处理下一个任务
+	}
+}
+
+//生产者线程
+void producer()
+{
+	int i = 1;
+	while (i < 50)
+	{
+		char name[31] = "your_name";
+		ReadTarget rt(name, 1, i);
+		{
+			std::unique_lock <std::mutex> lck(mtx);//用这个锁区访问共享变量,num和safequeue,cv不知道是不是
+			num++;
+			safequeue.enqueue(rt);
+			cv.notify_one();//唤醒一个线程
+		}
+		i++;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	std::thread consumers[6];//创建10个生产者和10个消费者
+	for (int i = 0; i < 6; i++)
+	{
+		consumers[i] = std::thread(consumer);
+		//consumers[i].join();
+	}
+	std::thread producers(producer);//直接创建了一个生产者线程
+
+	producers.join();
+	for (int i = 0; i < 6; i++)
+	{
+		consumers[i].join();
+	}
+	//spawn 10 consumers and 10 producers
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
